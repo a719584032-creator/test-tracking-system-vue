0d29816539a52ce5054456faeb8ad30670ed41ff
ysq <719584032@qq.com>
2025-09-19 17:10:15 +0800

    删除无效文件

--- src/api/models.js
+++
@@ -1,8 +0,0 @@
-import http from './http'
-
-export const modelsApi = {
-  // 根据计划ID获取机型列表
-  getModelNames(planId) {
-    return http.get(`/get_model_names/${planId}`)
-  }
-}
--- src/api/sheets.js
+++
@@ -1,8 +0,0 @@
-import http from './http'
-
-export const sheetsApi = {
-  // 根据计划ID获取用例表列表
-  getSheetNames(planId) {
-    return http.get(`/get_sheet_names/${planId}`)
-  }
-}
